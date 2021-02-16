6993cc298e735f2356e08cca56afb25c36d7e6ad
Cezary Wrzalik <cwrzalik@gmail.com>
2021-02-16 16:02:30 +0100

    dodano projekt

---
+++ source/repos/webapp2/webapp2/WebForm1.aspx.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+namespace webapp2
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SqlConnection stormconn = new SqlConnection("Server=tcp:projektazureserver.database.windows.net,1433;Initial Catalog=projektAzureDB;Persist Security Info=False;User ID=kilof;Password=Malina1256;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            stormconn.Open();
+            SqlCommand cmd = new SqlCommand("SELECT note FROM [dbo].[notes]");
+            cmd.CommandType = System.Data.CommandType.Text;
+            cmd.Connection = stormconn;
+
+            string temp = "";
+
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                temp += reader["note"].ToString();
+                temp += "<br/>";
+            }
+
+            stormconn.Close();
+
+            lbl_test.Text = temp;
+                if(Page.IsPostBack == true)
+            {
+                Label1.Text = ("Dodano notatkę");
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection stormconn = new SqlConnection("Server=tcp:projektazureserver.database.windows.net,1433;Initial Catalog=projektAzureDB;Persist Security Info=False;User ID=kilof;Password=Malina1256;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            {
+                SqlCommand insert = new SqlCommand("EXEC dbo.InsertFullnote @Fullnote", stormconn);
+                insert.Parameters.AddWithValue("@Fullnote", TextBox1.Text);
+
+                stormconn.Open();
+                insert.ExecuteNonQuery();
+                stormconn.Close();
+                stormconn.Open();
+                SqlCommand cmd = new SqlCommand("SELECT note FROM [dbo].[notes]");
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.Connection = stormconn;
+
+                string temp = "";
+
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    temp += reader["note"].ToString();
+                    temp += "<br/>";
+                }
+
+                stormconn.Close();
+
+                lbl_test.Text = temp;
+                if ( IsPostBack)
+                {
+                    TextBox1.Text = "";
+                }
+            }
+        }
+    }
+}
---
+++ source/repos/webapp2/webapp2/obj/Release/Package/PackageTmp/WebForm1.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="webapp2.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label1" runat="server" Text="Dodaj swoja notatkę"></asp:Label>
+        </div>
+        <p>
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Dodaj" />
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        </p>
+    </form>
+    <asp:Label ID="lbl_test" runat="server"></asp:Label>
+</body>
+</html>
